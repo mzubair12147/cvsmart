fba6bf07e44879de1d60e61f947e7a3c45685128
Muhammad Zubair <muhammadzubair12147@gmail.com>
2025-07-18 11:09:54 +0500

    Make some changes url's inside analyzer app, removed some static files.

--- cvsmart/analyzer/urls.py
+++ cvsmart/analyzer/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path("upload", views.upload_resume_view, name="upload_resume"),
     path("upload_success/", views.upload_success, name="upload_success"),
-    path("result/<int:resume_id>/", views.resume_analysis_result, name="analysis_result"),
+    path("result/<int:resume_id>/", views.resume_analysis_result, name="resume_analysis_result"),
     path("history/", views.history_view, name="history")
 ]
